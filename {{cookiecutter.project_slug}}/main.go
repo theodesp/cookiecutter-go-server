14d196757b352c1936d3b63ce468865e2996841b
Theo <theo.despoudis@teckro.com>
2018-01-07 10:30:13 +0000

    HTTP server config defaults

--- {{cookiecutter.project_slug}}/main.go
+++ {{cookiecutter.project_slug}}/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}/routes"
 	"github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}/utils"
 	"github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}/config"
+	"time"
 )
 
 func main()  {
@@ -14,6 +15,15 @@ func main()  {
 	utils.AttachRoutes(mux, routes.Routes)
 	config := config.LoadConfig()
 
-	log.Fatal(http.ListenAndServe(config.Host + ":" + config.Port, mux))
+	server := &http.Server{
+		Addr:           config.Host + ":" + config.Port,
+		Handler:        mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
 
